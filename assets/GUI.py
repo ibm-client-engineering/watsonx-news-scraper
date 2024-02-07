5e683d209e5e1610e066ccafab37425dd99c8f33
SamLee-IBM <Samuel.l@ibm.com>
2024-02-07 11:50:31 -0500

    Made the analysis data horizational

--- assets/GUI.py
+++ assets/GUI.py
@@ -30,7 +30,15 @@ def refresh() :
 def run_wx_single(df, i) :
    st.session_state['analysis_run'][i] = 1
    do_single_llm(df, i)
-   
+
+def render_df() :
+   datfram = pd.DataFrame(df.iloc[st.session_state['article_id'], 4:12]).T
+   _, num_cols = datfram.shape
+   sentiment_to_emoji = {"Falling": ":arrow-down-small:", "Decrease": ":arrow-down-small:", "Worse": ":arrow-down-small:", "Worst": ":arrow-down-small:", "Increase": ":arrow-up-small:", "Rising": ":arrow-up-small:", "Better": ":arrow-up-small:"} 
+   for i in range(num_cols) :
+      if datfram.iloc[0, i] in sentiment_to_emoji.keys() :
+         datfram.iloc[0, i] = datfram.iloc[0, i] + " " + sentiment_to_emoji[datfram.iloc[0, i]]
+   st.dataframe(datfram)
 
 # tab1, tab2 = st.tabs(['CNN', 'CNBC'])
 if 'selected' not in st.session_state :
@@ -71,7 +79,8 @@ if st.session_state['button_clicked'] :
       st.subheader(df.iloc[st.session_state['article_id']]['Title'])
       st.write(df.iloc[st.session_state['article_id']]['Text'])
       if st.session_state['analysis_run'][st.session_state['article_id']] == 1 :
-         st.dataframe(df.iloc[st.session_state['article_id'], 4:12])
+         #render_df()
+         st.dataframe(pd.DataFrame(df.iloc[st.session_state['article_id'], 4:12]).T.reset_index(drop=True))
       else :
          st.button("Run Watsonx Analysis", on_click=run_wx_single, args=(df, st.session_state['article_id']))
       
